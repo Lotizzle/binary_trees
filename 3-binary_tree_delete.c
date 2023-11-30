e3b0aeb1d45991e2471b7ac66631d80e09f7f51c
Lotizzle <lotharmbaegbu@gmail.com>
2023-11-30 04:45:02 -0800

    Deleted binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,11 +2,11 @@
 
 /**
  * binary_tree_delete - Deletes an entire binary tree
- * 
+ *
  * @tree: a pointer to the root node of the tree to delete
  */
 
- void binary_tree_delete(binary_tree_t *tree)
+void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
