be4a0f250fb0cd1e0fa678e0c004312ed006fee9
Lotizzle <lotharmbaegbu@gmail.com>
2023-11-29 19:05:22 -0800

    Inserted left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,12 +11,15 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *new__node;
 
-	if (parent == NULL || new == NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	new = binary_tree_node(parent, value);
+
+	if (new == NULL)
+		return (NULL);
 
 	if (parent->left != NULL)
 	{
